8263f57f5edbfcaf2f0a5d109a1792d2cab2c720
Craig Kelly <craig.n.kelly@gmail.com>
2016-03-22 16:19:13 -0500

    loop counts in send_power script

--- lirc-conf/send_power.sh
+++ lirc-conf/send_power.sh
@@ -11,6 +11,18 @@ if [ "$(id -u)" != "0" ]; then
     exit 1
 fi
 
+LOOP_COUNT=$1
+if [ "x$LOOP_COUNT" == "x" ]; then
+    echo "Using default loop count of 1"
+    LOOP_COUNT=1
+else
+    RE_NUMBERS='^[0-9]+$'
+    if ! [[ $LOOP_COUNT =~ $RE_NUMBERS ]] ; then
+       echo "error: loop count not a number" >&2
+       exit 1
+    fi
+fi
+
 
 # Configuration
 LIRCD_DEV=/dev/ttyUSB0
@@ -28,9 +40,11 @@ sleep 1  # Hack to make sure everything OK
 echo "...started lircd at PID $LIRCD_PID"
 
 # Actual send
-echo "Using irsend for key_power"
-irsend -d $IRSEND_DEV SEND_ONCE $REMOTE key_power
-sleep 1  # Hack to make sure everything OK
+echo "Using irsend for key_power $LOOP_COUNT times"
+for ((i = 1; i <= $LOOP_COUNT; i++)); do
+    irsend -d $IRSEND_DEV SEND_ONCE $REMOTE key_power
+    sleep 1  # Hack to make sure everything OK
+done
 
 # stop lircd
 echo "Stopping lircd PID $LIRCD_PID..."
